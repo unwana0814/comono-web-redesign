a06556f455dafd2d0a5be97fe3aa152be1dcd512
Unwana <brettygeorge3@gmail.com>
2023-01-24 21:21:43 +0000

    Added some animations and transition

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html, js}"],
+  content: ["./**/*.{html, js}"],
   theme: {
-    extend: {},
+    extend: {
+      transition: {
+        'left': 'scale-x'
+    },
+    },
   },
   plugins: [],
 }
