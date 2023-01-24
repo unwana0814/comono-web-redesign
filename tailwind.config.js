35322aa8f445292b4d55a89463e4d77ee3fd7378
Unwana <brettygeorge3@gmail.com>
2023-01-24 23:07:45 +0000

    Added some animations and transition

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,48 @@ module.exports = {
   content: ["./**/*.{html, js}"],
   theme: {
     extend: {
-      transition: {
-        'left': 'scale-x'
-    },
+        keyframes:
+      {
+        'trans-right': {
+          '0%' : {transform: 'translateX(100%)'},
+          '100%': {transform: 'translateX(0)'}
+        },
+
+        'trans-left': {
+          '0%' : {transform: 'translateX(-100%)'},
+          '100%': {transform: 'translateX(0)'}
+        },
+
+        'trans-bottom': {
+          '0%' : {transform: 'translateY(100%)'},
+          '100%': {transform: 'translateY(0)'}
+        },
+
+        'trans-top': {
+          '0%' : {transform: 'translateY(-100%)'},
+          '100%': {transform: 'translateY(0)'}
+        },
+
+        'left': {
+          '0%, 100%' : {transform: 'translateX(-10px) rotate(12deg)'},
+          '50%': {transform: 'translateX(0)'}
+        },
+
+        'right': {
+          '0%, 100%' : {transform: 'translateX(10px)'},
+          '50%': {transform: 'translateX(0)'}
+        },
+      },
+
+      animation: {
+        'trans-right' : 'trans-right 1.5s ease-in-out',
+        'trans-left' : 'trans-left 1.5s ease-in-out',
+        'trans-top' : 'trans-top 1.5s ease-in-out',
+        'trans-bottom' : 'trans-bottom 1.5s ease-in-out',
+        'left' : 'right 1.5s ease-in-out',
+        'right' : 'left 1.5s ease-in-out',
+
+      },
     },
   },
   plugins: [],
